2fc62705fa65058f210ea10d3b10092537cbfe73
Alex Masch <10alexff@gmail.com>
2022-01-16 20:42:20 +0200

    Find_number_to_win

--- Program.cs
+++ Program.cs
@@ -17,6 +17,23 @@ namespace lab3z2
             long newSeed = 0;
             long result = casino.Start_game(id); // посилаю запит на отримання справжнього числа
 
+
+            List<long> resultList = new List<long>();
+
+            for (int i = -100; i < 100; i++)
+            {
+                newSeed = mt + i;
+                RandomMersenne randomMersenne = new RandomMersenne((uint)newSeed);
+                for (int j = 0; j < 626; j++)
+                {
+                    resultList.Add(randomMersenne.Random());
+                }
+            }
+            int index = resultList.IndexOf(result);  // знаходжу це число в послідовності
+            long Next_result = resultList[index + 1]; // наступне число - передбачено
+            Console.WriteLine("resultat:   " + casino.Return_Namber(Next_result, id)); // перевірка на збіг
+            Console.WriteLine("money:   " + casino.Return_Money(id)); // виграш
+            Console.ReadKey();
         }
     }
 }
